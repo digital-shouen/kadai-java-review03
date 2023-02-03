72382de7d7fae16b12a0319eb484b36c47619fe5
digital-shouen <shouen.hisamatsu@gmail.com>
2023-02-03 21:13:14 +0900

    レッスン５課題再々提出

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -1,56 +1,55 @@
 package baseball;
 
 public class BaseBallTeam {
-    
     private String name;
     private int win;
     private int lose;
     private int draw;
-    //private double report;
-
-    public BaseBallTeam() {       
-    }
-   
-    public BaseBallTeam(String name,int win,int lose,int draw,double report) {
-        this.name = name;
-        this.win = win;
-        this.lose = lose;
-        this.draw = draw;
+    
+    public BaseBallTeam(String name, int win, int lose, int draw) {
+        setName(name);
+        setWin(win);
+        setLose(lose);
+        setDraw(draw);
     }
     
-    public String getName() {
-        return this.name;
+    String getName() {
+        return name;
     }
     
-    public void setName(String name) {
+    void setName(String name) {
         this.name = name;
     }
     
-    public int getWin() {
-        return this.win;
+    int getWin() {
+        return win;
     }
     
-    public void setWin(int win) {
+    void setWin(int win) {
         this.win = win;
     }
     
-    public int getLose() {
-        return this.lose;
+    int getLose() {
+        return lose;
     }
     
-    public void setLose(int lose) {
+    void setLose(int lose) {
         this.lose = lose;
     }
     
-    public int getDraw() {
-        return this.draw;
+    int getDraw() {
+        return draw;
     }
     
-    public void setDraw(int draw) {
+    void setDraw(int draw) {
         this.draw = draw;
     }
     
-    public double getReport() {
-        return(double)this.win / (this.win + this.lose);
+    double getRate() {
+        return(double)getWin() / (getWin()+getLose());
+    }
+    
+    public void report() {
+        System.out.println(this.name +"の2022年の成績は" + this.win + "勝" + this.lose + "敗" + this.draw + "分、勝率は" + getRate() + "です。" );
     }
 }
